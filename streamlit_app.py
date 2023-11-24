ca19983023b23203b9eda928cf2a25e51e45b469
omzh2720 <147129735+omzh2720@users.noreply.github.com>
2023-11-24 14:44:29 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,21 @@
-import openai
 import streamlit as st
 import openai
 
 st.set_page_config(page_title="プログラミングサポートAI", layout="wide")
 
-st.title("探究サポートAI")
+st.title("英語教師を超すAIです")
 st.caption("Created by Daiki Ito")
- 
+st.caption("Created by human")
+
 st.write('初めての方は右のリンクから「Sign up」をしてAPIキーを取得してください',
          'https://beta.openai.com'
          '/account/api-keys')
 # 質問フォーム
 with st.form(key='input_form'):
-    st.write("こんにちは！探究に関する質問をしてください。")
+    st.write("こんにちは！英語に関する質問をしてください。")
     st.write("質問は具体的なほうが的確にアドバイスをくれます。")
-    input_prompt = '''あなたは研究アシスタントです。ユーザは高校生で、あなたに研究に関する質問を投げかけます。
-    高校の教員として、論文執筆や研究の遂行に役立つ回答をわかりやすく、長文で、可能な限り根拠や実験方法や斬新なアイデアをステップバイステップで示した上で以下の######内の質問に返してください。'''
+    st.write("日本語と英語の区別をつけるためにかっこをつけてください。")
+    input_prompt = '''あなたは英語学習アシスタントです。ユーザは高校生で、あなたに英語に関する質問を投げかけます。あなたは高校の英語教師の代わりに高校の教員として、英語の日本語訳や文法、論理構造に関する回答をわかりやすく、長文で、可能な限り根拠を示して以下の######内の質問に返してください。'''
     input_apikey = st.text_input("取得したAPIキーを貼り付けてください")
     input_text = st.text_area("質問を入力してください")
     submitted = st.form_submit_button('質問する')
@@ -36,5 +36,3 @@ if submitted:
  
     st.write("返答:", response['choices'][0]['text'])
  
-st.write('差し支えなければアンケートへのご協力をお願いします。→', 'https://forms.gle/ADcoMh1zQUnoZVub7', 'まで')
-st.write('© 2022-2023 Daiki Ito. All Rights Reserved.')
