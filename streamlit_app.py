82ddf8bb84f8cc5f2e7e8f187fc7dd26f0db15c3
omzh2817 <147129704+omzh2817@users.noreply.github.com>
2023-11-24 14:45:54 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,37 @@
 import streamlit as st
 import openai
 
+ 
+
 st.set_page_config(page_title="プログラミングサポートAI", layout="wide")
 
-st.title("英語教師を超すAIです")
-st.caption("Created by Daiki Ito")
-st.caption("Created by human")
+ 
 
+st.title("英語教師を超すAIです")
+st.caption("Created by human")
+ 
 st.write('初めての方は右のリンクから「Sign up」をしてAPIキーを取得してください',
          'https://beta.openai.com'
          '/account/api-keys')
+
+ 
+
 # 質問フォーム
 with st.form(key='input_form'):
     st.write("こんにちは！英語に関する質問をしてください。")
     st.write("質問は具体的なほうが的確にアドバイスをくれます。")
     st.write("日本語と英語の区別をつけるためにかっこをつけてください。")
-    input_prompt = '''あなたは英語学習アシスタントです。ユーザは高校生で、あなたに英語に関する質問を投げかけます。あなたは高校の英語教師の代わりに高校の教員として、英語の日本語訳や文法、論理構造に関する回答をわかりやすく、長文で、可能な限り根拠を示して以下の######内の質問に返してください。'''
+    input_prompt = '''あなたは英語学習アシスタントです。ユーザは高校生で、あなたに英語に関する質問を投げかけます。あなたは高校の英語教師の代わりに高校の教員として、英語の日本語訳や文法、論理構造に関する回答をわかりやすく、長文で、可能な限り根拠を示して以下の######内の質問に返してください。もし、同じ質問をされた場合2倍の文章量で回答してください。'''
     input_apikey = st.text_input("取得したAPIキーを貼り付けてください")
     input_text = st.text_area("質問を入力してください")
     submitted = st.form_submit_button('質問する')
+
  
+
+ 
+
+ 
+
 if submitted:
     with st.spinner("考え中…"):
         openai.api_key = input_apikey
@@ -33,6 +45,11 @@ if submitted:
             frequency_penalty=0,
             presence_penalty=0
         )
+
  
+
+ 
+
+ 
+
     st.write("返答:", response['choices'][0]['text'])
- 
